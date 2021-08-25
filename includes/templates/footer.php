216fbc3a05324d7ab17e92e8ef36014b753821f2
ahmedibrahiim287 <49068254+ahmedibrahiim287@users.noreply.github.com>
2021-08-25 23:21:26 +0200

    qaa
    qaa

---
+++ includes/templates/footer.php
@@ -0,0 +1,10 @@
+  <footer class="page__footer">
+        <p>Ahmed ibrahim 2021&#169; </p>
+    </footer>
+
+
+    <script src="js/app.js"></script>
+
+</body>
+
+</html>
---
+++ includes/templates/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ahmed ibrahim cv</title>
+    <link rel="icon" href="" type="image/icon type">
+
+    <!-- Load Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@700&display=swap" rel="stylesheet">
+    <!-- Load Styles -->
+    <link href="css/styles.css" rel="stylesheet">
+    <link href="css/new-style.css" rel="stylesheet">
+</head>
+
+<body>
