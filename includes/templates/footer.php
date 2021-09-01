fd50b48814d73c9ebff6fe77f81a46da08c4437b
ahmedibrahiim287 <49068254+ahmedibrahiim287@users.noreply.github.com>
2021-09-01 09:59:12 +0200

    new ver
    new ver

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,9 +1,9 @@
-  <footer class="page__footer">
-        <p>Ahmed ibrahim 2021&#169; </p>
-    </footer>
+<footer class="page__footer">
+    <p>Ahmed ibrahim 2021&#169; </p>
+  </footer>
 
-
-    <script src="js/app.js"></script>
+  <!--import js files-->
+  <script src="js/app.js"></script>
 
 </body>
 
--- includes/templates/header.php
+++ includes/templates/header.php
@@ -2,19 +2,17 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ahmed ibrahim cv</title>
-    <link rel="icon" href="images/2.png" type="image/icon type">
-
-    <!-- Load Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@700&display=swap" rel="stylesheet">
-    <!-- Load Styles -->
-    <link href="css/styles.css" rel="stylesheet">
-    <link href="css/new-style.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Ahmed ibrahim cv</title>
+  <link rel="icon" href="img/2.png" type="image/icon type">
+  <!-- Load Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@700&display=swap" rel="stylesheet">
+  <!-- Load Styles -->
+  <link href="css/styles.css" rel="stylesheet">
 </head>
 
 <body>
