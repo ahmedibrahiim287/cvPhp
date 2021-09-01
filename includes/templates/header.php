0cdf0ec50fbe81c5bdc98df8025b6b5d1f1b8e7c
ahmedibrahiim287 <49068254+ahmedibrahiim287@users.noreply.github.com>
2021-09-01 10:10:03 +0200

    image2

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Ahmed ibrahim cv</title>
-  <link rel="icon" href="img/2.png" type="image/icon type">
+  <link rel="icon" href="images/2.png" type="image/icon type">
   <!-- Load Google Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
